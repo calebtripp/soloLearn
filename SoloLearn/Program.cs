17721250ed77668c6448839e712f7f97e2adfd25
calebtripp <calebtripp@outlook.com>
2022-09-13 13:13:50 -0400

    cleaning up repos

---
+++ SoloLearn/Program.cs
@@ -0,0 +1,43 @@
+﻿////const double pi = 3.14;
+//double radius;
+
+//Console.WriteLine("What is the radius of the circle?");
+
+// didnt work double r = Convert.ToDouble(Console.ReadLine());
+
+//radius = Convert.ToDouble(Console.ReadLine());
+
+//double arEa;
+//arEa = (Math.Pow(radius,2)*pi);
+
+
+
+//radius = Convert.ToInt32(Console.ReadLine());
+//radius = (Console.ReadLine());
+// do I have the order wrong here somewhere? I keep getting conversion or type errors?
+// earlier trial, didnt work radius = Convert.ToDouble(Console.ReadLine()); //do I need to Convert.ToDouble?? its already one, right?
+//double arEa = (radius * pi);
+
+//Console.WriteLine("The area of your circle is" (arEa));
+
+
+//var n = 13;
+
+//new 
+//$
+//Math.Pow (radius,2)
+// didnt work double r = Convert.ToDouble(Console.ReadLine());
+const double pi = 3.14;
+double radius;
+
+Console.WriteLine("What is the radius of the circle?");
+
+radius = Convert.ToDouble(Console.ReadLine());
+
+double arEa = ((radius * radius) * pi);
+
+Console.WriteLine($"The area of the circle is {arEa}");
+//why doesnt this work? - didn't have the right brackets {}
+ //Console.WriteLine("The area of the circle is");
+
+//Console.WriteLine(arEa);
